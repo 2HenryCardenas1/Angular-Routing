eacd1d992d8bb58d4439b9b040da75c612df8194
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-01-23 20:24:59 -0500

    components product-detail created and implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './page/home/home.component';
 import { LoginComponent } from './page/login/login.component';
 import { MycartComponent } from './page/mycart/mycart.component';
 import { NotFoundComponent } from './page/not-found/not-found.component';
+import { ProductDetailComponent } from './page/product-detail/product-detail.component';
 import { ProfileComponent } from './page/profile/profile.component';
 import { RecoveryComponent } from './page/recovery/recovery.component';
 import { RegisterComponent } from './page/register/register.component';
@@ -23,6 +24,10 @@ const routes: Routes = [
   {
     path: 'categories/:id',
     component: CategoryComponent
+  },
+  {
+    path: 'product-detail/:id',
+    component: ProductDetailComponent
   },
   {
     path: 'login',
